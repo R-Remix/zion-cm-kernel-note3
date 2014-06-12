80fef717da34ec42ade752ceb996c9e756913700
ZION959 <zion959@gmail.com>
2014-06-12 14:32:26 -0700

    REMOVE: cpufreq governor SmartAssv2 (thanks to @Erasmux, @faux123 & @androdguide) (reverted from commit dfd8a05bfdd546d1028d982f90d570cf622cacef)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -478,9 +478,6 @@ extern struct cpufreq_governor cpufreq_gov_dancedance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSV2)
-extern struct cpufreq_governor cpufreq_gov_smartass2;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass2)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_nightmare)
